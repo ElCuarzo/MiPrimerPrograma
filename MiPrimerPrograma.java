a998bca1a8215310c6d5a004576709aa8271dcc6
ElCuarzo <Diegolobos77@gmail.com>
2024-03-17 23:19:34 -0300

    MiPrimerPrograma

---
+++ MiPrimerPrograma.java
@@ -0,0 +1,7 @@
+public class MiPrimerPrograma {
+    public static void main(String[] args) {
+        System.out.println("Mi nombre es Diego Lobos");
+        System.out.println("Tengo 23 años de edad");
+        System.out.println("Mi ciudad es Concepción");
+    }
+}
